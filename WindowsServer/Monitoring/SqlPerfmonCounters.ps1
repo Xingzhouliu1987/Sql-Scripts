5afde9368a2973f17983efbc82cfc01d36f7c447
Xing Liu <xing@xin.gl>
2017-07-25 20:26:11 -0700

    SqlPerfmon
    Not Yet Complete!

---
+++ WindowsServer/Monitoring/SqlPerfmonCounters.ps1
@@ -0,0 +1,15 @@
+﻿Param(
+  [string]$name = "MyCollectorSet",
+  [string]$root = "%systemdrive%\PerfLogs\Admin\",
+  [int]$duration = 0
+)
+
+$dc = New-Object -COM Pla.DataCollectorSet
+
+$dc.DisplayName = $name;
+$dc.Duration = 0;
+$dc.SubdirectoryFormat = 1;
+$dc.SubdirectoryFormatPattern = "yyyy\mm";
+$dc.RootPath = $root + "\" + $name ;
+
+$Dc = $dc.DataCollectors.CreateDataCollector(0)
